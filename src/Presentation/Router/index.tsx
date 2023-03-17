0a922d6ee695247d8caf1ec9175e7a8a8d96ad34
bruch0 <lucas.bruch0@gmail.com>
2023-03-17 19:17:29 -0300

    feat: add register page to router

--- src/Presentation/Router/index.tsx
+++ src/Presentation/Router/index.tsx
@@ -2,12 +2,17 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import GlobalStyles from "../Components/Shared/GlobalStyles";
 
 import LoginPage from "@Pages/Login";
+import RegisterPage from "@Pages/Register";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <LoginPage />,
+    },
+    {
+      path: "/register",
+      element: <RegisterPage />,
     },
   ]);
 
