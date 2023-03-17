a7ce95fae996335733cd1960a602e6d6b3e51c90
bruch0 <lucas.bruch0@gmail.com>
2023-03-17 19:17:04 -0300

    feat: add react-toastify component to root

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import Router from "@Router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router />
+    <>
+      <ToastContainer
+        hideProgressBar={true}
+        autoClose={1000}
+        theme="colored"
+        pauseOnHover={false}
+        closeOnClick={true}
+      />
+      <Router />
+    </>
   </React.StrictMode>
 );
