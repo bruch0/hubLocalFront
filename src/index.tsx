fc13a0da5fd791d0b73526f87093a4d85a09b412
bruch0 <lucas.bruch0@gmail.com>
2023-03-20 10:45:22 -0300

    feat: add modal provider

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import { ReactToastifyUserFeedback } from "@Frameworks/Feedback/react-toastfy";
+import { ReactModal } from "@Frameworks/Modal/react-modal";
 
 import Router from "@Router";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <>
-      {new ReactToastifyUserFeedback().provider()}
-      <Router />
-    </>
+    {new ReactModal().provider(
+      <>
+        {new ReactToastifyUserFeedback().provider()}
+        <Router />
+      </>
+    )}
   </React.StrictMode>
 );
