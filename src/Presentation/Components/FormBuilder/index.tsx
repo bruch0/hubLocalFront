5c10e1e0a6bd8afa89dc65845376458801e6dc09
bruch0 <lucas.bruch0@gmail.com>
2023-03-20 13:05:28 -0300

    feat: formbuilder now supports modals and masks

--- src/Presentation/Components/FormBuilder/index.tsx
+++ src/Presentation/Components/FormBuilder/index.tsx
@@ -1,80 +1,161 @@
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 
-import { Form, Label, Input, Error, Submit } from "./styles";
+import {
+  Form,
+  Label,
+  Input,
+  Error,
+  Submit,
+  InputHolder,
+  ModalBottom,
+  InputContainer,
+  MaskedInput,
+} from "./styles";
 
 interface FormData {
   inputs: Array<{
     name: string;
     label: string;
     defaultValue?: string;
-    patern?: RegExp;
+    pattern?: RegExp;
     errorMessage: string;
     required: boolean;
     disabled: boolean;
     type: string;
+    nested: boolean;
     validatePassword?: boolean;
+    mask?: string;
   }>;
-  submitButton: string;
+  submitButton: {
+    value: string;
+    height: string;
+    width: string;
+    fontSize: string;
+  };
   onSubmit: Function;
+  modalBottom?: boolean;
 }
 
 export default function App({ formData }: { formData: FormData }) {
   const {
     register,
+    control,
     handleSubmit,
     watch,
     formState: { errors, isSubmitting },
   } = useForm();
 
+  const { inputs, submitButton, modalBottom } = formData;
+
   const onSubmit = (data: any): void => formData.onSubmit(data);
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      {formData.inputs.map((userInput) => (
-        <>
-          <Label htmlFor={userInput.name}>{userInput.label}</Label>
-          <Input
-            {...register(userInput.name, {
-              required: userInput.required,
-              pattern: userInput.patern,
-              value: userInput.defaultValue,
-              validate: userInput.validatePassword
-                ? (value: string) => {
-                    if (watch("password") !== value) {
-                      return "Your passwords do no match";
-                    }
-                  }
-                : undefined,
-            })}
-            type={userInput.type}
-          />
-          {errors[userInput.name] && <Error>{userInput.errorMessage}</Error>}
-        </>
-      ))}
-
-      <Submit type="submit" disabled={isSubmitting}>
-        {isSubmitting ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="white"
-          >
-            <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                dur="0.75s"
-                values="0 12 12;360 12 12"
-                repeatCount="indefinite"
+      <InputContainer modal={!!modalBottom}>
+        {inputs.map((userInput) => (
+          <InputHolder nested={userInput.nested}>
+            <Label htmlFor={userInput.name}>{userInput.label}</Label>
+            {userInput.mask ? (
+              <Controller
+                name={userInput.name}
+                control={control}
+                rules={{
+                  required: userInput.required,
+                  pattern: userInput.pattern,
+                  value: userInput.defaultValue,
+                }}
+                render={({ field }) => (
+                  <MaskedInput
+                    mask="99.999.999/9999-99"
+                    onChange={field.onChange}
+                  />
+                )}
               />
-            </path>
-          </svg>
-        ) : (
-          formData.submitButton
-        )}
-      </Submit>
+            ) : (
+              <Input
+                {...register(userInput.name, {
+                  required: userInput.required,
+                  pattern: userInput.pattern,
+                  value: userInput.defaultValue,
+                  validate: userInput.validatePassword
+                    ? (value: string) => {
+                        if (watch("password") !== value) {
+                          return "Your passwords do no match";
+                        }
+                      }
+                    : undefined,
+                })}
+                type={userInput.type}
+              />
+            )}
+            {errors[userInput.name] && <Error>{userInput.errorMessage}</Error>}
+          </InputHolder>
+        ))}
+      </InputContainer>
+
+      {modalBottom ? (
+        <ModalBottom>
+          <Submit
+            type="submit"
+            disabled={isSubmitting}
+            height={submitButton.height}
+            width={submitButton.width}
+            fontSize={submitButton.fontSize}
+          >
+            {isSubmitting ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="white"
+              >
+                <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
+                  <animateTransform
+                    attributeName="transform"
+                    type="rotate"
+                    dur="0.75s"
+                    values="0 12 12;360 12 12"
+                    repeatCount="indefinite"
+                  />
+                </path>
+              </svg>
+            ) : (
+              submitButton.value
+            )}
+          </Submit>
+        </ModalBottom>
+      ) : (
+        <Submit
+          type="submit"
+          disabled={isSubmitting}
+          height={submitButton.height}
+          width={submitButton.width}
+          fontSize={submitButton.fontSize}
+        >
+          {isSubmitting ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="white"
+            >
+              <path d="M10.14,1.16a11,11,0,0,0-9,8.92A1.59,1.59,0,0,0,2.46,12,1.52,1.52,0,0,0,4.11,10.7a8,8,0,0,1,6.66-6.61A1.42,1.42,0,0,0,12,2.69h0A1.57,1.57,0,0,0,10.14,1.16Z">
+                <animateTransform
+                  attributeName="transform"
+                  type="rotate"
+                  dur="0.75s"
+                  values="0 12 12;360 12 12"
+                  repeatCount="indefinite"
+                />
+              </path>
+            </svg>
+          ) : (
+            submitButton.value
+          )}
+        </Submit>
+      )}
     </Form>
   );
 }
--- src/Presentation/Components/FormBuilder/styles.ts
+++ src/Presentation/Components/FormBuilder/styles.ts
@@ -1,9 +1,30 @@
 import styled from "styled-components";
+import InputMask from "react-input-mask";
 
 const Form = styled.form`
   width: 100%;
   display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  width: 100%;
+  padding: ${(props: { modal: boolean }) => (props.modal ? "25px" : "0px")};
+`;
+
+const InputHolder = styled.div`
+  display: flex;
   flex-direction: column;
+  width: ${(props: { nested: boolean }) => (props.nested ? "45%" : "100%")};
+  margin-bottom: 20px;
+
+  :last-of-type {
+    margin: 0px;
+  }
 `;
 
 const Label = styled.label`
@@ -25,25 +46,55 @@ const Input = styled.input`
   margin: 0px;
   padding: 0px 10px;
   font-size: 25px;
+  text-overflow: ellipsis;
+`;
+
+const MaskedInput = styled(InputMask)`
+  height: 50px;
+  color: black;
+  border: 1px solid #0385fd;
+  border-radius: 5px;
+  margin: 0px;
+  padding: 0px 10px;
+  font-size: 25px;
+  text-overflow: ellipsis;
+`;
+
+interface SubmitProps {
+  height: string;
+  width: string;
+  fontSize: string;
+}
+
+const Submit = styled.button<SubmitProps>`
+  height: 70px;
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  color: white;
+  border: 0px;
+  background-color: #0385fd;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: ${(props) => props.fontSize};
+  font-weight: 600;
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
 
   :last-of-type {
     margin-top: 20px;
   }
 `;
 
-const Submit = styled.button`
-  height: 70px;
-  color: white;
-  border: 0px;
-  background-color: #0385fd;
-  border-radius: 5px;
-  cursor: pointer;
-  font-size: 25px;
-  text-transform: uppercase;
-  font-weight: 600;
+const ModalBottom = styled.div`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0px 20px;
+  box-shadow: 0px -2px 4px rgba(0, 0, 0, 0.25);
 
-  :last-of-type {
-    margin-top: 20px;
+  button {
+    margin: 0px !important;
   }
 `;
 
@@ -52,4 +103,14 @@ const Error = styled.span`
   color: red;
 `;
 
-export { Form, Label, Input, Error, Submit };
+export {
+  Form,
+  InputContainer,
+  InputHolder,
+  Label,
+  Input,
+  MaskedInput,
+  Error,
+  ModalBottom,
+  Submit,
+};
